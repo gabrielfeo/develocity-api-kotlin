5c283ed190e9fe4b413343ff04963820963250f3
Gabriel Feo <gabriel@gabrielfeo.com>
2022-12-15 18:18:04 +0000

    Use retrofit to avoid 'exploded' bug
    generator lacks 'exploded' support when library=jvm-okhttp4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ openApiGenerate {
     modelPackage.set("com.gradle.enterprise.api.model")
     packageName.set("com.gradle.enterprise.api.client")
     invokerPackage.set("com.gradle.enterprise.api.client")
+    additionalProperties.put("library", "jvm-retrofit2")
 }
 
 sourceSets {
@@ -46,4 +47,8 @@ dependencies {
     implementation("com.squareup.moshi:moshi:1.13.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 }
--- app/src/main/kotlin/com/gabrielfeo/gradle/enterprise/api/app/template/App.kt
+++ app/src/main/kotlin/com/gabrielfeo/gradle/enterprise/api/app/template/App.kt
@@ -1,4 +1,15 @@
 package com.gabrielfeo.gradle.enterprise.api.app.template
 
+import com.gradle.enterprise.api.GradleEnterpriseApi
+import com.gradle.enterprise.api.client.infrastructure.ApiClient
+
 fun main() {
+    val apiClient = ApiClient(
+        baseUrl = "https://gradle-enterprise.ifoodcorp.com.br",
+        bearerToken = getTokenFromKeychain("ifood-ge-api-token"),
+        authName = "GradleEnterpriseAccessKey",
+    )
+    val api = apiClient.createService(GradleEnterpriseApi::class.java)
+    val builds = api.getBuilds(since = 0, maxBuilds = 3).execute().body()!!
+    builds.forEach(::println)
 }
