22f1335872ac0a6d1a08d802ebd087b5a094ebad
Gabriel Feo <gabriel@gabrielfeo.com>
2022-12-16 12:49:43 +0000

    Target Java 18 to avoid reflection warning
    square/retrofit#3448

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,12 @@ sourceSets {
     }
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    }
+}
+
 dependencies {
     implementation("com.squareup.moshi:moshi:1.13.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
