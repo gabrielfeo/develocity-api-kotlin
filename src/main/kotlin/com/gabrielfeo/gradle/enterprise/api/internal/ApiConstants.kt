f87cb291a3098cb76681cb2fa72ebfbe99f46b68
Gabriel Feo <gabriel@gabrielfeo.com>
2023-01-03 13:40:27 +0000

    Refactor Api extensions

---
+++ src/main/kotlin/com/gabrielfeo/gradle/enterprise/api/internal/ApiConstants.kt
@@ -0,0 +1,6 @@
+package com.gabrielfeo.gradle.enterprise.api.internal
+
+/**
+ * Undocumented max value of `/api/builds?maxBuilds`. Last checked in 2022.4.
+ */
+internal const val API_MAX_BUILDS = 1000
---
+++ src/main/kotlin/com/gabrielfeo/gradle/enterprise/api/internal/operator/pagedUntilLastBuild.kt
@@ -0,0 +1,34 @@
+package com.gabrielfeo.gradle.enterprise.api.internal.operator
+
+import com.gabrielfeo.gradle.enterprise.api.*
+import com.gabrielfeo.gradle.enterprise.api.internal.API_MAX_BUILDS
+import com.gabrielfeo.gradle.enterprise.api.model.*
+import kotlinx.coroutines.flow.*
+import retrofit2.await
+
+/**
+ * Emits all available builds starting from the upstream Flow builds until the last build available.
+ * Makes paged requests to the API using `fromBuild`, [maxPerRequest] at a time.
+ */
+internal fun Flow<Build>.pagedUntilLastBuild(
+    maxPerRequest: Int,
+): Flow<Build> {
+    val firstBuilds = this
+    return flow {
+        var lastBuildId = ""
+        firstBuilds.collect {
+            lastBuildId = it.id
+            emit(it)
+        }
+        if (lastBuildId.isEmpty()) {
+            return@flow
+        } else while (true) {
+            val builds = api.getBuilds(fromBuild = lastBuildId, maxBuilds = maxPerRequest).await()
+            emitAll(builds.asFlow())
+            when {
+                builds.isEmpty() || builds.size < API_MAX_BUILDS -> break
+                else -> lastBuildId = builds.last().id
+            }
+        }
+    }
+}
