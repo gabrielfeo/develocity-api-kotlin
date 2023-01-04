771733f5cb22c7d41fbd05a6ffd7ceb61b45a4de
Gabriel Feo <gabriel@gabrielfeo.com>
2023-01-04 15:45:46 +0000

    Improve sample

--- sample.main.kts
+++ sample.main.kts
@@ -1,9 +1,41 @@
 @file:Repository("https://jitpack.io")
-@file:DependsOn("com.github.gabrielfeo:gradle-enterprise-api-kotlin:0.4")
+@file:DependsOn("com.github.gabrielfeo:gradle-enterprise-api-kotlin:0.9")
+
+/*
+ * Counts how many developers don't run tests on their local machine
+ */
 
 import com.gabrielfeo.gradle.enterprise.api.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import java.time.*
 
-val builds = api.getBuilds(since = 0, maxBuilds = 3).execute().body()!!
-builds.forEach {
-    println(it)
+val oneMonthAgo = LocalDate.now()
+    .minusMonths(1)
+    .atStartOfDay()
+    .toInstant(ZoneOffset.UTC)
+    .toEpochMilli()
+
+runBlocking {
+
+    // Filter builds from the API
+    val buildsByUser = api.getGradleAttributesFlow(since = oneMonthAgo)
+        .filter { "CI" !in it.tags }
+        .toList()
+        .groupBy { it.environment.username }
+    check(buildsByUser.isNotEmpty()) { "No builds found!" }
+
+    // Count users
+    val userCount = buildsByUser.size
+    val userCountDoesntRunTestsLocally = buildsByUser.count { (_, userBuilds) ->
+        userBuilds.none { build ->
+            build.requestedTasks.any { task -> "test" in task.lowercase() }
+        }
+    }
+
+    // Present result
+    val percent = "%.2f".format(userCountDoesntRunTestsLocally / userCount.toDouble() * 100)
+    print("$percent% of developers don't run tests on their local machine")
+    shutdown()
+
 }
