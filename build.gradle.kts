db1a68eedd744f61f5d853fdad0ce291c3c1c494
Gabriel Feo <gabriel@gabrielfeo.com>
2023-01-04 19:32:55 +0000

    Bump moshi version

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,8 +111,8 @@ publishing {
 }
 
 dependencies {
-    api("com.squareup.moshi:moshi:1.13.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+    api("com.squareup.moshi:moshi:1.14.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     api("com.squareup.okhttp3:okhttp:4.10.0")
     api("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
