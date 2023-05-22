529f39928c9e62dae621552e14171b12fb1321bd
Gabriel Feo <gabriel@gabrielfeo.com>
2023-05-22 21:24:58 +0100

    Fix extension test

--- library/src/test/kotlin/com/gabrielfeo/gradle/enterprise/api/model/BuildAttributesValueExtensionsTest.kt
+++ library/src/test/kotlin/com/gabrielfeo/gradle/enterprise/api/model/BuildAttributesValueExtensionsTest.kt
@@ -1,6 +1,7 @@
 package com.gabrielfeo.gradle.enterprise.api.model
 
 import com.gabrielfeo.gradle.enterprise.api.extension.contains
+import com.gabrielfeo.gradle.enterprise.api.extension.get
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
