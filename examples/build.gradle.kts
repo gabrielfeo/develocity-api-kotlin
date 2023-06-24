5a5e5b9ed2c7e03c81ae713388fe99dc9038b312
Gabriel Feo <gabriel@gabrielfeo.com>
2023-06-24 18:15:27 +0100

    Move example script

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -6,13 +6,21 @@ plugins {
 
 val exampleTestTasks = ArrayList<TaskProvider<*>>()
 
-exampleTestTasks += tasks.register<Exec>("runExampleScript") {
-    group = "Application"
-    description = "Runs the 'example-script.main.kts' script"
-    commandLine("kotlinc", "-script", file("example-script.main.kts"))
-    environment("JAVA_OPTS", "-Xmx1g")
+// Add tasks to run each example script
+val scripts = fileTree(file("example-scripts"))
+exampleTestTasks += scripts.map { scriptFile ->
+    val scriptName = scriptFile.name.substringBefore(".main.kts")
+    @Suppress("DEPRECATION")
+    val camelCaseName = scriptName.split("-").joinToString("") { it.capitalize() }
+    tasks.register<Exec>("run${camelCaseName}Script") {
+        group = "Application"
+        description = "Runs the '${scriptFile.name}' script"
+        commandLine("kotlinc", "-script", scriptFile.path)
+        environment("JAVA_OPTS", "-Xmx1g")
+    }
 }
 
+// Add a task to run the example-project
 exampleTestTasks += tasks.register<GradleBuild>("runExampleProject") {
     group = "Application"
     description = "Runs examples/example-project as a standalone build"
@@ -20,10 +28,10 @@ exampleTestTasks += tasks.register<GradleBuild>("runExampleProject") {
     tasks = listOf("run")
 }
 
+// Add tasks to run each example notebook
 val notebooks = fileTree(file("example-notebooks")) {
     exclude(".ipynb_checkpoints")
 }
-
 exampleTestTasks += notebooks.map { notebook ->
     val buildDir = project.layout.buildDirectory.asFile.get()
     tasks.register<Exec>("run${notebook.nameWithoutExtension}Notebook") {
@@ -39,6 +47,7 @@ exampleTestTasks += notebooks.map { notebook ->
     }
 }
 
+// Add a lifecycle task to run all the test tasks
 val runAll = tasks.register("runAll") {
     group = "Application"
     description = "Runs everything in 'examples' directory"
