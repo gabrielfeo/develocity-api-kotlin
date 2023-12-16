fa4dc70e8e20f2e7744849f33a8b609afae6598c
Gabriel Feo <gabriel@gabrielfeo.com>
2023-12-16 01:26:49 +0000

    Add failing test

---
+++ library/src/integrationTest/kotlin/com/gabrielfeo/gradle/enterprise/api/GradleEnterpriseApiIntegrationTest.kt
@@ -0,0 +1,43 @@
+package com.gabrielfeo.gradle.enterprise.api
+
+import com.gabrielfeo.gradle.enterprise.api.internal.*
+import com.gabrielfeo.gradle.enterprise.api.internal.FakeKeychain
+import com.gabrielfeo.gradle.enterprise.api.internal.RealEnv
+import com.gabrielfeo.gradle.enterprise.api.internal.RealKeychain
+import com.gabrielfeo.gradle.enterprise.api.internal.RealSystemProperties
+import com.gabrielfeo.gradle.enterprise.api.internal.env
+import com.gabrielfeo.gradle.enterprise.api.internal.keychain
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.assertDoesNotThrow
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GradleEnterpriseApiIntegrationTest {
+
+    @Test
+    fun canFetchBuildsWithDefaultConfig() = runTest {
+        env = RealEnv
+        keychain = RealKeychain(RealSystemProperties)
+        val api = GradleEnterpriseApi.newInstance()
+        val builds = api.buildsApi.getBuilds(
+            since = 0,
+            maxBuilds = 5,
+            query = """tag:local value:"Email=gabriel.feo*""""
+        )
+        assertEquals(5, builds.size)
+        api.shutdown()
+    }
+
+    @Test
+    fun canBuildNewInstanceWithPureCodeConfiguration() = runTest {
+        env = FakeEnv()
+        keychain = FakeKeychain()
+        assertDoesNotThrow {
+            val config = Config(
+                apiUrl = "https://google.com/api/",
+                apiToken = { "" },
+            )
+            GradleEnterpriseApi.newInstance(config)
+        }
+    }
+}
