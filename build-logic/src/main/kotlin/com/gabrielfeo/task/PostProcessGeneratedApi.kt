65601417e5ac375a23befddec289dc6df7585338
Gabriel Feo <gabriel@gabrielfeo.com>
2024-04-04 20:44:08 +0100

    Squashed changes
    In reverse:
    * 8e1b024a Update generated-api-diff paths for new task
    * ae354cc9 Delete obsolete workaround
    * e3b83d08 Add more tests
    * 1c0231d8 Refactor
    * 280156a9 Refactor
    * 141b386f Refactor
    * cea6921f Draft test for task

---
+++ build-logic/src/main/kotlin/com/gabrielfeo/task/PostProcessGeneratedApi.kt
@@ -0,0 +1,99 @@
+package com.gabrielfeo.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+import org.gradle.kotlin.dsl.withGroovyBuilder
+import java.io.File
+import javax.inject.Inject
+
+@CacheableTask
+abstract class PostProcessGeneratedApi @Inject constructor(
+    private val fsOperations: FileSystemOperations,
+) : DefaultTask() {
+
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val originalFiles: DirectoryProperty
+
+    @get:Input
+    abstract val modelsPackage: Property<String>
+
+    @get:OutputDirectory
+    abstract val postProcessedFiles: DirectoryProperty
+
+    @TaskAction
+    fun doWork() {
+        postProcessedFiles.get().asFile.deleteRecursively()
+        fsOperations.copy {
+            from(originalFiles)
+            into(postProcessedFiles)
+        }
+        postProcess(
+            srcDir = postProcessedFiles.get().asFile,
+            modelsPackage = modelsPackage.get()
+        )
+    }
+
+    private fun postProcess(srcDir: File, modelsPackage: String) {
+        // Replace Response<X> with X in every method return type of GradleEnterpriseApi.kt
+        ant.withGroovyBuilder {
+            "replaceregexp"(
+                "match" to ": Response<(.*?)>$",
+                "replace" to """: \1""",
+                "flags" to "gm",
+            ) {
+                "fileset"(
+                    "dir" to srcDir,
+                    "includes" to "com/gabrielfeo/gradle/enterprise/api/*Api.kt",
+                )
+            }
+        }
+        // Add @JvmSuppressWildcards to avoid square/retrofit#3275
+        ant.withGroovyBuilder {
+            "replaceregexp"(
+                "match" to "interface",
+                "replace" to """
+                    @JvmSuppressWildcards
+                    interface
+                """.trimIndent(),
+                "flags" to "m",
+            ) {
+                "fileset"(
+                    "dir" to srcDir,
+                    "includes" to "com/gabrielfeo/gradle/enterprise/api/*Api.kt",
+                )
+            }
+        }
+        // Workaround for missing imports of exploded queries
+        val escapedModelPackage = modelsPackage.replace(".", "\\.")
+        val lastModelImportInFilePattern = """(?:import $escapedModelPackage.[.\w]+\s)+"""
+        ant.withGroovyBuilder {
+            "replaceregexp"(
+                "match" to lastModelImportInFilePattern,
+                // Import all models instead: current + missing ones
+                "replace" to "import $modelsPackage.*\n",
+                "flags" to "m",
+            ) {
+                "fileset"(
+                    "dir" to srcDir
+                )
+            }
+        }
+        // Fix mapping of BuildModelName: gradle-attributes -> gradleAttributes
+        ant.withGroovyBuilder {
+            "replaceregexp"(
+                "match" to "Minus",
+                "replace" to "",
+                "flags" to "mg",
+            ) {
+                "fileset"(
+                    "dir" to srcDir,
+                    "includes" to "com/gabrielfeo/gradle/enterprise/api/model/BuildModelName.kt",
+                )
+            }
+        }
+    }
+}
