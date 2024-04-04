c267ad56f8f531856d2e940c08d6bea1a58b7480
Gabriel Feo <gabriel@gabrielfeo.com>
2024-04-04 20:46:40 +0100

    Reformat

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -16,7 +16,6 @@ gradlePlugin {
     testSourceSets(sourceSets["functionalTest"])
 }
 
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     implementation("org.openapitools:openapi-generator-gradle-plugin:7.3.0")
