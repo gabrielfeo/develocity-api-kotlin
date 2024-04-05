8c52e0a8b6aa162fcd2343664606644be295612a
Gabriel Feo <gabriel@gabrielfeo.com>
2024-04-05 18:03:40 +0100

    Test all public classes imported

--- library/src/integrationTest/kotlin/com/gabrielfeo/develocity/api/internal/jupyter/DevelocityApiJupyterIntegrationTest.kt
+++ library/src/integrationTest/kotlin/com/gabrielfeo/develocity/api/internal/jupyter/DevelocityApiJupyterIntegrationTest.kt
@@ -1,10 +1,18 @@
 package com.gabrielfeo.develocity.api.internal.jupyter
 
+import com.gabrielfeo.develocity.api.DevelocityApi
+import com.google.common.reflect.ClassPath
+import com.google.common.reflect.ClassPath.ClassInfo
+import kotlinx.coroutines.test.runTest
 import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlinx.jupyter.api.Code
 import org.jetbrains.kotlinx.jupyter.testkit.JupyterReplTestCase
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.javaType
 import kotlin.test.Test
 
+@ExperimentalStdlibApi
 class DevelocityApiJupyterIntegrationTest : JupyterReplTestCase() {
 
     @Test
@@ -23,6 +31,23 @@ class DevelocityApiJupyterIntegrationTest : JupyterReplTestCase() {
     fun `imports extensions package`() = assertSucceeds("""
         com.gabrielfeo.develocity.api.BuildsApi::getBuildsFlow
     """)
+
+    @Test
+    fun `imports all library classes`() {
+        val classes = allPublicClassesRecursive("com.gabrielfeo.develocity.api")
+        val references = classes.joinToString("\n") { "${it.name}::class" }
+        println("Running code:\n$references")
+        assertSucceeds(references)
+    }
+
+    @Suppress("SameParameterValue")
+    private fun allPublicClassesRecursive(packageName: String): List<ClassInfo> {
+        val cp = ClassPath.from(this::class.java.classLoader)
+        return cp.getTopLevelClassesRecursive(packageName)
+            .filter { "internal" !in it.packageName }
+            .filter { !it.name.endsWith("Kt") }
+            .filter { Class.forName(it.name).kotlin.visibility == KVisibility.PUBLIC }
+    }
 
     private fun assertSucceeds(@Language("kts") code: Code) {
         code.lines().forEach {
