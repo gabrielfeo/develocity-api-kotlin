567ade8a58d284e6450c6ca3c41dd32ccf0e274f
Gabriel Feo <gabriel@gabrielfeo.com>
2024-04-05 22:39:07 +0100

    Replace version in README and examples

--- examples/example-scripts/example-script.main.kts
+++ examples/example-scripts/example-script.main.kts
@@ -17,7 +17,7 @@
  * Run this with at least 1GB of heap to accomodate the fetched data: JAVA_OPTS=-Xmx1g
  */
 
-@file:DependsOn("com.gabrielfeo:develocity-api-kotlin:2023.4.0")
+@file:DependsOn("com.gabrielfeo:develocity-api-kotlin:2024.1.0")
 
 import com.gabrielfeo.develocity.api.*
 import com.gabrielfeo.develocity.api.model.*
