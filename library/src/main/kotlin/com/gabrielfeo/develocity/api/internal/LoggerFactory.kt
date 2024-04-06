3b59112fcdf549781247e7a61f1ecf9c6d1a5a73
Gabriel Feo <gabriel@gabrielfeo.com>
2024-04-07 00:08:16 +0100

    Replace logback for simple-logger

--- library/src/main/kotlin/com/gabrielfeo/develocity/api/internal/LoggerFactory.kt
+++ library/src/main/kotlin/com/gabrielfeo/develocity/api/internal/LoggerFactory.kt
@@ -2,7 +2,6 @@ package com.gabrielfeo.develocity.api.internal
 
 import com.gabrielfeo.develocity.api.Config
 import org.slf4j.Logger
-import java.io.File
 import kotlin.reflect.KClass
 
 interface LoggerFactory {
@@ -13,33 +12,12 @@ class RealLoggerFactory(
     private val config: Config,
 ) : LoggerFactory {
 
-    private val loggingConfig by lazy {
-        val level = checkNotNull(config.logLevel)
-        File.createTempFile("develocity-api-kotlin-logging-config", ".xml").apply {
-            writeText(LOGGING_CONFIG_TEMPLATE.replace("{{{LEVEL}}}", level))
-        }
-    }
-
     override fun newLogger(cls: KClass<*>): Logger {
-        config.logLevel?.let {
-            System.setProperty("logback.configurationFile", loggingConfig.absolutePath)
+        if (System.getProperty(SIMPLE_LOGGER_LOG_LEVEL) == null) {
+            System.setProperty(SIMPLE_LOGGER_LOG_LEVEL, config.logLevel ?: "off")
         }
         return org.slf4j.LoggerFactory.getLogger(cls.java)
     }
 }
 
-private const val LOGGING_CONFIG_TEMPLATE = """
-<configuration>
-	<appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
-		<!-- encoders are assigned the type
-			 ch.qos.logback.classic.encoder.PatternLayoutEncoder by default -->
-		<encoder>
-			<pattern>%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} -%kvp- %msg%n</pattern>
-		</encoder>
-	</appender>
-
-	<root level="{{{LEVEL}}}">
-		<appender-ref ref="STDOUT" />
-	</root>
-</configuration>
-"""
+private const val SIMPLE_LOGGER_LOG_LEVEL = "org.slf4j.simpleLogger.defaultLogLevel"
