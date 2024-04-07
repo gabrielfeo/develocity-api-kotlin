f0a470be199198225753f6860a1981ff211ccfbc
Gabriel Feo <gabriel@gabrielfeo.com>
2024-04-07 01:28:25 +0100

    Bump version for next release (#205)

--- examples/example-scripts/example-script.main.kts
+++ examples/example-scripts/example-script.main.kts
@@ -17,7 +17,7 @@
  * Run this with at least 1GB of heap to accomodate the fetched data: JAVA_OPTS=-Xmx1g
  */
 
-@file:DependsOn("com.gabrielfeo:develocity-api-kotlin:2024.1.0")
+@file:DependsOn("com.gabrielfeo:develocity-api-kotlin:2024.1.1")
 
 import com.gabrielfeo.develocity.api.*
 import com.gabrielfeo.develocity.api.model.*
