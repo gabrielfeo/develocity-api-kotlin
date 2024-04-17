87b3381e203041d951bc20e97117d6dab72c0d09
Gabriel Feo <gabriel@gabrielfeo.com>
2024-04-17 12:18:05 +0100

    Update usage of Develocity plugin, wrapper-validation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version("3.17.2")
+    id("com.gradle.develocity") version("3.17.2")
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
@@ -19,10 +19,9 @@ dependencyResolutionManagement {
     }
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        publishAlways()
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+        termsOfUseAgree = "yes"
     }
 }
