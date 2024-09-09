f30181b59f6e068c482e79414844f32bc54eb7c3
Gabriel Feo <gabriel@gabrielfeo.com>
2024-09-09 19:50:15 +0100

    Bump Gradle to 8.10.1 (#300)
    - `check` build: https://scans.gradle.com/s/nt7sxd46mlipo
    - `integrationTest` retry: https://scans.gradle.com/s/yl6vxthpbo7ew

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,5 +1,9 @@
 @file:Suppress("UnstableApiUsage")
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
