56838ddd66fd862e295fc5098fbdd2ce9fe61920
Gabriel Féo <gabriel@gabrielfeo.com>
2025-08-14 22:01:18 +0100

    Remove Kotlin/Jupyter Gradle plugin (#447)
    Remove Kotlin Jupyter Gradle plugin in order to avoid its reliance on
    dev versions of the Kotlin Gradle plugin (and the Kotlin compiler). The
    original motivation for this was one Kotlin/Jupyter version that was
    relying on an unpublished KGP version, [failing builds][1] on #424.
    
    <img width="1090" height="212" alt="Screenshot 2025-08-14 at 20 25 41"
    src="https://github.com/user-attachments/assets/8df3fee4-a2e6-4053-b323-1244daa6459e"
    />
    
    That's since been resolved, but beta versions of KGP keep being used and
    I'd rather avoid publishing a library built with pre-release versions of
    KGP.
    
    <img width="781" height="518" alt="Screenshot 2025-08-14 at 20 25 01"
    src="https://github.com/user-attachments/assets/062f3c7d-759f-487e-8d60-62eafed8ce71"
    />
    
    The integration that was previously facilitated by the plugin is now
    done manually. `examplesTest` guarantees this is functional.
    
    [1]: https://scans.gradle.com/s/vy3gdlvqaolni

--- library/src/main/kotlin/com/gabrielfeo/develocity/api/internal/jupyter/DevelocityApiJupyterIntegration.kt
+++ library/src/main/kotlin/com/gabrielfeo/develocity/api/internal/jupyter/DevelocityApiJupyterIntegration.kt
@@ -1,13 +1,16 @@
 package com.gabrielfeo.develocity.api.internal.jupyter
 
-import org.jetbrains.kotlinx.jupyter.api.libraries.JupyterIntegration
+import org.jetbrains.kotlinx.jupyter.api.KotlinKernelVersion
+import org.jetbrains.kotlinx.jupyter.api.libraries.LibraryDefinition
 
 @Suppress("unused")
-class DevelocityApiJupyterIntegration : JupyterIntegration() {
+class DevelocityApiJupyterIntegration : LibraryDefinition {
 
-    override fun Builder.onLoaded() {
-        import("com.gabrielfeo.develocity.api.*")
-        import("com.gabrielfeo.develocity.api.model.*")
-        import("com.gabrielfeo.develocity.api.extension.*")
-    }
+    override val minKernelVersion = KotlinKernelVersion.from("0.12.0.217")
+
+    override val imports = listOf(
+        "com.gabrielfeo.develocity.api.*",
+        "com.gabrielfeo.develocity.api.model.*",
+        "com.gabrielfeo.develocity.api.extension.*",
+    )
 }
