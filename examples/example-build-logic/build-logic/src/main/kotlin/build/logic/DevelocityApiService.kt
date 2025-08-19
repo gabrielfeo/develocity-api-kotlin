a704a53da76b5657e05d253ce0368288be9577a0
Gabriel Feo <gabriel@gabrielfeo.com>
2025-08-19 20:44:00 +0100

    Fix "executor rejected" error if shutdown is used
    See https://github.com/gabrielfeo/develocity-api-kotlin/issues/451. Was reproducible with
    
    ```
    ./gradlew --stop; for i in 1 2; ./g
    radlew -p examples/example-build-logic/ buildPerformanceMetrics --stacktrace; end
    ```

--- examples/example-build-logic/build-logic/src/main/kotlin/build/logic/DevelocityApiService.kt
+++ examples/example-build-logic/build-logic/src/main/kotlin/build/logic/DevelocityApiService.kt
@@ -4,9 +4,10 @@ import com.gabrielfeo.develocity.api.Config
 import com.gabrielfeo.develocity.api.DevelocityApi
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
+import okhttp3.OkHttpClient
 
 abstract class DevelocityApiService
-    : DevelocityApi by DevelocityApi.newInstance(),
+    : DevelocityApi by DevelocityApi.newInstance(config()),
       BuildService<BuildServiceParameters.None>,
       AutoCloseable {
 
@@ -14,3 +15,9 @@ abstract class DevelocityApiService
         shutdown()
     }
 }
+
+private fun config() = Config(
+    // Necessary to accomodate Gradle's build service lifecycle
+    // See https://github.com/gabrielfeo/develocity-api-kotlin/issues/451
+    clientBuilder = OkHttpClient.Builder(),
+)
