afff394edd7fe2f6e4488134039e68e2133dd93f
Gabriel Feo <gabriel@gabrielfeo.com>
2025-08-23 18:47:56 +0100

    Clarify magic numbers

--- library/src/main/kotlin/com/gabrielfeo/develocity/api/internal/auth/HostAccessKeyEntry.kt
+++ library/src/main/kotlin/com/gabrielfeo/develocity/api/internal/auth/HostAccessKeyEntry.kt
@@ -6,11 +6,16 @@ internal class HostAccessKeyEntry(entry: String) {
 
     init {
         require(components.size == 2 && host.isNotBlank() && accessKey.isNotBlank()) {
-            val redactedEntry = if (entry.length <= 5) entry else "${entry.substring(0, 4)}[redacted]"
-            "Invalid access key entry format: '$redactedEntry'. Expected format is 'host=accessKey'."
+            "Invalid access key entry format: '${redact(entry)}'. Expected format is 'host=accessKey'."
         }
     }
 
     val host: String get() = components[0]
     val accessKey: String get() = components[1]
 }
+
+private const val REDACTED_MAX_LENGTH = 5
+
+private fun redact(entry: String): String =
+    if (entry.length <= REDACTED_MAX_LENGTH) entry
+    else "${entry.substring(0, REDACTED_MAX_LENGTH - 1)}[redacted]"
