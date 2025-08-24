1feeaf827371012a8c13e4d0b72b3543ff866ffd
Gabriel Feo <gabriel@gabrielfeo.com>
2025-08-24 03:22:42 +0100

    Refactor example-project test to use init script

--- library/src/examplesTest/kotlin/com/gabrielfeo/develocity/api/example/gradle/ExampleProjectTest.kt
+++ library/src/examplesTest/kotlin/com/gabrielfeo/develocity/api/example/gradle/ExampleProjectTest.kt
@@ -2,6 +2,9 @@ package com.gabrielfeo.develocity.api.example.gradle
 
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Path
@@ -9,6 +12,7 @@ import kotlin.io.path.div
 import com.gabrielfeo.develocity.api.example.copyFromResources
 import com.gabrielfeo.develocity.api.example.runInShell
 
+@TestMethodOrder(OrderAnnotation::class)
 class ExampleProjectTest {
 
     @TempDir
@@ -17,35 +21,40 @@ class ExampleProjectTest {
     private val projectDir
         get() = tempDir / "examples/example-project"
 
+    private val initScriptPath
+        get() = tempDir / ResourceInitScripts.FORCE_SNAPSHOT_LIBRARY
+
     @BeforeEach
     fun setup() {
         copyFromResources("/examples", tempDir)
-        forceUseOfMavenLocalSnapshotArtifact(projectDir / "build.gradle.kts")
+        copyFromResources("/${ResourceInitScripts.FORCE_SNAPSHOT_LIBRARY}", tempDir)
+    }
+
+    @Test
+    @Order(1)
+    fun smokeTest() {
+        val dependencies = runBuild("dependencies --configuration runtimeClasspath")
+        val libraryMatches = dependencies.lines().filter { "develocity-api-kotlin" in it }
+        assertTrue(libraryMatches.isNotEmpty())
+        assertTrue(libraryMatches.all { "-> SNAPSHOT" in it && "FAILED" !in it }) {
+            "Expected forced SNAPSHOT versions, but found [${libraryMatches.joinToString(", ")}]"
+        }
     }
 
     @Test
     fun testExampleProject() {
-        val output = runInShell(projectDir, "./gradlew --stacktrace --no-daemon run").trim()
+        val output = runBuild("run")
         val tableRegex = Regex("""(?ms)^[-]+\nMost frequent builds:\n\s*\n(.+\|\s*\d+\s*\n?)+""")
         assertTrue(tableRegex.containsMatchIn(output)) {
             "Expected match for pattern '$tableRegex' in output '$output'"
         }
     }
 
-    private fun forceUseOfMavenLocalSnapshotArtifact(buildGradlePath: Path) {
-        val repositoryDefinition = """
-            repositories {
-                exclusiveContent {
-                    forRepository { mavenLocal() }
-                    filter { includeGroup("com.gabrielfeo") }
-                }
-            }
-        """.trimIndent()
-        val original = buildGradlePath.toFile().readText()
-        val patched = original.replace(
-            Regex("""com.gabrielfeo:develocity-api-kotlin:[^"]+"""),
-            "com.gabrielfeo:develocity-api-kotlin:SNAPSHOT",
-        ) + repositoryDefinition
-        buildGradlePath.toFile().writeText(patched)
-    }
+    private fun runBuild(gradleArgs: String) =
+        runInShell(
+            projectDir,
+            "./gradlew --stacktrace --no-daemon",
+            "-I $initScriptPath",
+            gradleArgs,
+        )
 }
