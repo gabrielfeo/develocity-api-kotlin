512d55ce9d53699201cc74e467ede1de1d07e230
Gabriel Feo <gabriel@gabrielfeo.com>
2025-09-09 20:15:41 +0100

    WIP

--- build-logic/src/main/kotlin/com/gabrielfeo/examples-test-suite.gradle.kts
+++ build-logic/src/main/kotlin/com/gabrielfeo/examples-test-suite.gradle.kts
@@ -65,29 +65,26 @@ tasks.named("check") {
 }
 
 val enforceSameQueries by tasks.registering {
+    val allowedQueries = setOf(
+        """buildStartTime>-7d buildTool:gradle""",
+        """user:"${'$'}user" buildStartTime>${'$'}startTime"""
+    )
+    inputs.property("allowedQueries", allowedQueries)
     inputs.files(examples)
         .withPropertyName("exampleFiles")
         .withPathSensitivity(PathSensitivity.NONE)
         .skipWhenEmpty()
     outputs.cacheIf { true }
     doLast {
-        var firstQuery: Pair<File, String>? = null
         for (file in examples.files) {
             file.useLines { lines ->
-                val query = lines
-                    .map { it.trim(' ', ',') }
-                    .firstOrNull { Regex("""query\s*=""").containsMatchIn(it) }
-                    ?.removePrefix("query")
-                    ?.trim(' ', '=', '"', '\'')
+                val query = lines.firstOrNull { Regex("""query\s*=""").containsMatchIn(it) }
+                    ?.trim('\n', ' ', ',', '=', '"', '\'', '\\')
+                    ?.substringAfter("query")
+                    ?.trim('\n', ' ', ',', '=', '"', '\'', '\\')
                     ?: continue
-                if (firstQuery == null) {
-                    firstQuery = file to query
-                    continue
-                }
-                val (firstFile, firstQuery) = firstQuery
-                if (firstQuery != query) {
-                    val comparison = "'$firstQuery' ($firstFile) vs '$query' ($file)"
-                    throw VerificationException("Mismatched queries in examples: $comparison")
+                if (query !in allowedQueries) {
+                    throw VerificationException("Query not in allowedQueries: '$query' ($file). Allowed: $allowedQueries")
                 }
             }
         }
