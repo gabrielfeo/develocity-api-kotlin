8edf4346ee4f9cbb638ccb9215b04839f2d0e28e
Gabriel Feo <gabriel@gabrielfeo.com>
2025-09-13 16:56:18 +0100

    Make NotebooksTest run concurrently

--- library/src/examplesTest/kotlin/com/gabrielfeo/develocity/api/example/notebook/NotebooksTest.kt
+++ library/src/examplesTest/kotlin/com/gabrielfeo/develocity/api/example/notebook/NotebooksTest.kt
@@ -4,10 +4,11 @@ import com.gabrielfeo.develocity.api.copyFromResources
 import com.gabrielfeo.develocity.api.example.JsonAdapter
 import com.gabrielfeo.develocity.api.example.Queries
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.io.TempDir
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT
 import java.net.URI
 import java.nio.file.Path
 import kotlin.io.path.Path
@@ -15,32 +16,15 @@ import kotlin.io.path.absolute
 import kotlin.io.path.div
 import kotlin.io.path.writeText
 
+@Execution(CONCURRENT)
 class NotebooksTest {
 
-    @TempDir
-    lateinit var tempDir: Path
-
-    lateinit var venv: PythonVenv
-
-    lateinit var jupyter: Jupyter
-
-    @BeforeEach
-    fun setup() {
-        copyFromResources("/examples", tempDir)
-        venv = PythonVenv(tempDir / ".venv").also {
-            it.installRequirements(
-                requirementsFile = tempDir / "examples/example-notebooks/requirements.txt",
-                linksDir = Path(System.getProperty("downloaded-requirements-path")).absolute(),
-            )
-        }
-        jupyter = Jupyter(tempDir, venv.dir)
-    }
-
     @Test
-    fun testMostFrequentBuildsNotebook() {
+    fun testMostFrequentBuildsNotebook(@TempDir tempDir: Path) {
+        val jupyter = setup(tempDir)
         val sourceNotebook = tempDir / "examples/example-notebooks/MostFrequentBuilds.ipynb"
-        val fasterNotebook = forceUseOfFasterQuery(sourceNotebook)
-        val snapshotNotebook = forceUseOfMavenLocalSnapshotArtifact(fasterNotebook)
+        val fasterNotebook = jupyter.forceUseOfFasterQuery(sourceNotebook)
+        val snapshotNotebook = jupyter.forceUseOfMavenLocalSnapshotArtifact(fasterNotebook)
         val executedNotebook = assertDoesNotThrow { jupyter.executeNotebook(snapshotNotebook) }
         with(JsonAdapter.fromJson(executedNotebook.outputNotebook).asNotebookJson()) {
             assertTrue(textOutputLines.any { Regex("""Collected \d+ builds from the API""").containsMatchIn(it) }) {
@@ -55,27 +39,39 @@ class NotebooksTest {
         }
     }
 
-    private fun forceUseOfFasterQuery(sourceNotebook: Path): Path = jupyter.replacePattern(
+    private fun setup(tempDir: Path): Jupyter {
+        copyFromResources("/examples", tempDir)
+        val venv = PythonVenv(tempDir / ".venv").also {
+            it.installRequirements(
+                requirementsFile = tempDir / "examples/example-notebooks/requirements.txt",
+                linksDir = Path(System.getProperty("downloaded-requirements-path")).absolute(),
+            )
+        }
+        return Jupyter(tempDir, venv.dir)
+    }
+
+    private fun Jupyter.forceUseOfFasterQuery(sourceNotebook: Path): Path = replacePattern(
         path = sourceNotebook,
         pattern = Regex("""query\s*=.+,"""),
         replacement = """query = "${Queries.FAST}",""",
     )
 
     @Test
-    fun testLoggingNotebook() {
+    fun testLoggingNotebook(@TempDir tempDir: Path) {
+        val jupyter = setup(tempDir)
         val sourceNotebook = tempDir / "examples/example-notebooks/Logging.ipynb"
-        val snapshotNotebook = forceUseOfMavenLocalSnapshotArtifact(sourceNotebook)
+        val snapshotNotebook = jupyter.forceUseOfMavenLocalSnapshotArtifact(sourceNotebook)
         val executedNotebook = assertDoesNotThrow { jupyter.executeNotebook(snapshotNotebook) }
         val kernelLogs = executedNotebook.outputStreams.stderr
         assertTrue(kernelLogs.contains("gabrielfeo.develocity.api.Cache - HTTP cache", ignoreCase = true))
     }
 
-    private fun forceUseOfMavenLocalSnapshotArtifact(sourceNotebook: Path): Path {
+    private fun Jupyter.forceUseOfMavenLocalSnapshotArtifact(sourceNotebook: Path): Path {
         val mavenLocal = Path(System.getProperty("user.home"), ".m2/repository").toUri()
-        val libraryDescriptor = (tempDir / "develocity-api-kotlin.json").apply {
+        val libraryDescriptor = (workDir / "develocity-api-kotlin.json").apply {
             writeText(buildLibraryDescriptor(version = "SNAPSHOT", repository = mavenLocal))
         }
-        return jupyter.replacePattern(
+        return replacePattern(
             path = sourceNotebook,
             pattern = Regex("(?:DependsOn|%use).*develocity-api-kotlin.*"),
             replacement = """
@@ -86,6 +82,7 @@ class NotebooksTest {
         )
     }
 
+    @Suppress("SameParameterValue")
     private fun buildLibraryDescriptor(version: String, repository: URI) = """
         {
           "dependencies": ["com.gabrielfeo:develocity-api-kotlin:$version"],
