c9c39ba34d2f4eeaa562fbee53d3886e32818e94
Gabriel Féo <gabriel@gabrielfeo.com>
2025-10-02 00:21:57 +0100

    Fix SmokeTest

--- library/src/integrationTest/kotlin/com/gabrielfeo/develocity/api/SmokeTest.kt
+++ library/src/integrationTest/kotlin/com/gabrielfeo/develocity/api/SmokeTest.kt
@@ -8,7 +8,12 @@ import kotlin.test.assertTrue
 class SmokeTest {
 
     @Test
-    fun testSuiteNotSensitiveToEnvironment() {
-        assertTrue(RealEnv.asMap().isEmpty())
+    fun testSuiteEnvironmentIsEmpty() {
+        val vars = RealEnv.asMap().toMutableMap().entries.apply {
+            // Added by either Gradle or JUnit that but irrelevant to library functionality
+            removeAll { (k, _) -> k.endsWith("CF_USER_TEXT_ENCODING") }
+            removeAll { (k, _) -> k.startsWith("JAVA_MAIN_CLASS") }
+        }
+        assertTrue(vars.isEmpty(), "Expected empty environment, found $vars")
     }
 }
